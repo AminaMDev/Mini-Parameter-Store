295900017390869568d58bcc21910e563fafd21e
Amina M <amina.dev.m@gmail.com>
2022-09-28 17:21:39 -0700

    Added model for Parameter

--- parameterstore/api/models.py
+++ parameterstore/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Parameter(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+    value = models.CharField(max_length=60, blank=False)
+    flag = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
